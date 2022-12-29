8a2a9fc98daf86b73c54c29e87726f97b0b2fbf6
Sebastián <seba.chavez21@gmail.com>
2022-12-29 09:47:53 -0300

    modificaciones woe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  // apiUrl: 'https://api-pandemicro.com',
+  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://api-pandemicro.com',
   debug: true
 };
 
